487a2a7cd13118741ff48620f94321720b4b966b
sam-k0 <56673835+sam-k0@users.noreply.github.com>
2024-02-07 17:51:28 +0100

    Add module blacklisting

--- Filesystem.h
+++ Filesystem.h
@@ -2,6 +2,9 @@
 
 #include <string>
 #include <direct.h>
+#include <iostream>
+#include <fstream>
+
 // File functions
 namespace Filesys {
 
@@ -19,7 +22,7 @@ std::string GetCurrentDir() // Returns EXE directory
     return str;
 }
 
-inline bool FileExists(const std::string& name) {
+bool FileExists(const std::string& name) {
     if (FILE* file = fopen(name.c_str(), "r")) {
         fclose(file);
         return true;
@@ -27,6 +30,39 @@ inline bool FileExists(const std::string& name) {
     else {
         return false;
     }
+}
+// Read text file line by line
+std::vector<std::string> ReadFromFile(const std::string& filepath) {
+    std::vector<std::string> lines;
+    std::ifstream file(filepath);
+
+    if (!file.is_open()) {
+        return lines; // Return an empty vector if file cannot be opened
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        lines.push_back(line);
+    }
+
+    file.close();
+    return lines;
+}
+
+bool WriteToFile(const std::string& filepath, const std::vector<std::string>& lines) {
+    std::ofstream file(filepath, std::ofstream::trunc); // Open file for writing (truncate existing content)
+
+    if (!file.is_open()) {
+        
+        return false;
+    }
+
+    for (const auto& line : lines) {
+        file << line << std::endl;
+    }
+
+    file.close();
+    return true;
 }
 
 }
--- ModuleManager.h
+++ ModuleManager.h
@@ -2,12 +2,41 @@
 #include <fstream>
 #include <iterator>
 #include <map>
+#include "Filesystem.h"
+#include "MyHelper.h"
+
 std::map<std::string, YYTKPlugin*> gRegisteredPlugins;
 bool gReady = false;
+const std::string gModBlacklist = "DisabledMods.meta";
 
 bool Ready() // Plugins call this to see if the core is present
 {
     return gReady;
+}
+
+// Checks if the module is allowed or not
+bool ModuleAllowed(std::string modName)
+{  
+    std::string blacklistedModsPath = Filesys::GetCurrentDir() + "\\" + gModBlacklist;
+    if (Filesys::FileExists(blacklistedModsPath))
+    {
+        std::vector<std::string> blacklistedModsNames = Filesys::ReadFromFile(blacklistedModsPath);
+        if (Misc::VectorContains(modName, &blacklistedModsNames)) // Blacklisted
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    else
+    {
+        std::ofstream file(blacklistedModsPath); // Create file without specifying content
+        file.close();
+        return true;
+    }
+    
 }
 
 bool RegisterModule(std::string modName, YYTKPlugin* pluginHandle) // Plugins call this to register themselves to the core
@@ -19,10 +48,23 @@ bool RegisterModule(std::string modName, YYTKPlugin* pluginHandle) // Plugins ca
         return false;
     }
 
-    // add to map
-    gRegisteredPlugins.insert(std::pair<std::string, YYTKPlugin*>(modName, pluginHandle));
-    Misc::Print("Registered callbacks for mod: " + modName, CLR_GREEN);
-    return true;
+    // Check if allowed to load
+    if (ModuleAllowed(modName))
+    {
+        // add to map
+        gRegisteredPlugins.insert(std::pair<std::string, YYTKPlugin*>(modName, pluginHandle));
+        Misc::Print("Registered callbacks for mod: " + modName, CLR_GREEN);
+        return true;
+    }
+    else
+    {
+        Misc::Print("Unloaded plugin: " + modName, CLR_RED);
+        // not allowed to load
+        PmUnloadPlugin(pluginHandle->PluginStart);
+        return true; // maybe change this to give mod feedback
+    }
+
+    
 }
 
 bool UnregisterModule(std::string modName) // Plugins call this to say goodbye
